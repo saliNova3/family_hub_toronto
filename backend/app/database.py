4f0dcb9a6c657f50a12a61f878af74df1e225c77
Your Name <you@example.com>
2025-02-22 09:57:19 -0500

    "TLS/SSL certificate verification"

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,17 @@
 import motor.motor_asyncio
 import os
+import certifi  # For TLS/SSL certificate verification
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
+
 
 MONGO_DETAILS = os.getenv("MONGO_DETAILS")
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-database = client.family_hub_toronto
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    MONGO_DETAILS,
+    tls=True,
+    tlsCAFile=certifi.where()  
+)
+database = client["family_hub_toronto"]
+
 centres_collection = database.get_collection("toronto_family_centres")
